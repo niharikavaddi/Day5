b3a13e9913c52062c344f3f0f473737816e84e1a
VADDI LAKSHMI NEEHARIKA <36045615+niharikavaddi@users.noreply.github.com>
2020-11-13 22:38:12 +0530

    Update RunRate.java

--- Day5/src/com/service/RunRate.java
+++ Day5/src/com/service/RunRate.java
@@ -1,12 +1,8 @@
 package com.service;
 
 public class RunRate {
-	public void calculate(int runs, int overs) throws NumberFormatException, ArithmeticException {
-		try {
-			System.out.println(runs / overs);
-		} catch (Exception exp) {
-			System.err.println(exp.getClass());
-		}
+	public float calculate(int runs, int overs) throws ArithmeticException {
+			return (runs / overs);
 	}
 
 }
